c1308b023b57006812f716f261df4547b2431a90
Ashish Mohite <mohite.aashish.1@gmail.com>
2020-04-13 19:09:59 +0530

    Add testimonial

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // For testimonial slider
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    items:1,
+    nav:false,
+    dots:true,
+    autoplay:true,
+    smartSpeed: 1000,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true
+  });
+});
